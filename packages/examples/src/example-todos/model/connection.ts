dedfef996700f43267e6ed8db18756b4512f388d
Marcello Bastea-Forte <marcello@cellosoft.com>
2019-03-01 22:00:58 -0800

    pull out json-diff3 logic from client

--- packages/examples/src/example-todos/model/connection.ts
+++ packages/examples/src/example-todos/model/connection.ts
@@ -1,6 +1,6 @@
 import { Collabodux, Connection } from '@collabodux/client';
 import { ModelState, validateAndNormalize } from './model';
-import { JSONValue, Path } from 'json-diff3';
+import { diff3, JSONValue, Path } from 'json-diff3';
 import * as IoPaths from 'io-ts-path';
 import { MergableType } from '../../dux/io-ts-util';
 import { addUserManagement } from '../../dux/user-cleanup';
@@ -13,24 +13,27 @@ export const connection = new Connection(
 export const collabodux = new Collabodux(
   connection,
   validateAndNormalize,
-  {
-    handleMerge(
-      base: JSONValue | undefined,
-      left: JSONValue,
-      right: JSONValue,
-      path: Path,
-    ): JSONValue {
-      const type = IoPaths.type(ModelState, path);
-      console.log(`conflict at ${path} type=${type}: `, base, left, right);
-      if (type instanceof MergableType) {
-        return type.merge(base, left, right);
-      }
-      throw new Error(`cannot merge /${path.join('/')}`);
-    },
-    getArrayItemKey(item: any, index: number, arrayPath: Path): string {
-      return item.key;
-    },
-  },
+  (base, local, remote) =>
+    validateAndNormalize(
+      diff3(base, local, remote, {
+        handleMerge(
+          base: JSONValue | undefined,
+          left: JSONValue,
+          right: JSONValue,
+          path: Path,
+        ): JSONValue {
+          const type = IoPaths.type(ModelState, path);
+          console.log(`conflict at ${path} type=${type}: `, base, left, right);
+          if (type instanceof MergableType) {
+            return type.merge(base, left, right);
+          }
+          throw new Error(`cannot merge /${path.join('/')}`);
+        },
+        getArrayItemKey(item: any, index: number, arrayPath: Path): string {
+          return item.key;
+        },
+      }),
+    ),
   // for simulating high latency connections, set this:
   // 5 * 1000,
 );
